ea04aa14dec85fac6fadfe3cee3245d4781c4620
Kavishka Kalhara <kavikalhara9h@gmail.com>
2025-04-05 09:59:32 +0530

    package json updatd

--- src/redux/user/userSlice.d.ts
+++ src/redux/user/userSlice.d.ts
@@ -1,17 +1,23 @@
-// filepath: /D:/DevOps/SolveIt-frontend/src/redux/user/userSlice.d.ts declare module '../redux/user/userSlice' { import { AnyAction, ThunkAction } from '@reduxjs/toolkit';
+// userSlice.d.ts
 
-export interface UserState { currentUser: any; error: string | null; loading: boolean; }
+import { AnyAction } from 'redux';
 
-export const signInStart: () => AnyAction; 
-export const signInSuccess: (user: any) => AnyAction; 
-export const signInFailure: (error: string) => AnyAction; 
-export const updateStart: () => AnyAction; 
-export const updateSuccess: (user: any) => AnyAction; 
-export const updateFailure: (error: string) => AnyAction; 
-export const deleteUserStart: () => AnyAction; 
-export const deleteUserSuccess: () => AnyAction; 
-export const deleteUserFailure: (error: string) => AnyAction; 
+export interface UserState {
+  currentUser: any;
+  error: string | null;
+  loading: boolean;
+}
+
+export const signInStart: () => AnyAction;
+export const signInSuccess: (user: any) => AnyAction;
+export const signInFailure: (error: string) => AnyAction;
+export const updateStart: () => AnyAction;
+export const updateSuccess: (user: any) => AnyAction;
+export const updateFailure: (error: string) => AnyAction;
+export const deleteUserStart: () => AnyAction;
+export const deleteUserSuccess: () => AnyAction;
+export const deleteUserFailure: (error: string) => AnyAction;
 export const signoutSuccess: () => AnyAction;
 
-const userReducer: (state: UserState | undefined, action: AnyAction) => UserState; 
-export default userReducer; 
+export const userReducer: (state: UserState | undefined, action: AnyAction) => UserState;
+
