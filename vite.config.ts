4b7bdf96fbe42df6fc13833d55251b02f7d2fb4e
Kavishka Kalhara <kavikalhara9h@gmail.com>
2025-04-07 12:11:06 +0530

    Sign in fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,28 @@
-g
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5000,
+    host: true,
+    allowedHosts: true, // This will allow all hosts in development
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/userService/, ''),
+        secure: true,
+      },
+    },
+    hmr: {
+      clientPort: 443, // Add this if you're using HTTPS
+      host: 'www.solveitsl.site',
+    },
+  },
+  preview: {
+    host: true,
+    port: 5000,
+    allowedHosts: true, // This will allow all hosts in preview mode
+  },
+});
