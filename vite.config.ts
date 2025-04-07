5e776fa0ffba28281064bd59597a8229cb0a7ed2
Kavishka Kalhara <kavikalhara9h@gmail.com>
2025-04-07 11:25:08 +0530

    Sign in fixed

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/userService/, ''),
-        secure: false,
+        secure: true,
       },
     },
     hmr: {
